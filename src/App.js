1eabc8726e1f69019c0aa3b3ed690d2f47c3de4f
IlyaMinchenko <sir.over@yandex.ru>
2021-12-04 21:32:24 +0500

    2.0
    Update fetch.
    On-line update fly-information from flightradar24
    fix bugs

--- src/App.js
+++ src/App.js
@@ -3,30 +3,20 @@ import React from "react";
 import './App.css'
 import ReactMapGL, { Marker } from 'react-map-gl';
 import { ReactComponent as Airplane } from './marker.svg';
-import { d10, d50, d100, az10, az30} from "./circle";
-const qs = require('querystring')
-
+import { d10, d50, d100, az10, az30 } from "./circle";
 
 function App() {
-  const [data, setData] = useState('');
-  const [show, setShow] = useState(false)
-  let nameInput = React.createRef()
+  const [locBase, setLocBase] = useState([])
 
   useEffect(() => {
-    if (nameInput.current.value) {
-      zapros();
-    } else {
-      console.log('Введите номер самолета!')
-    }
+    locate()
   });
 
-  function zapros() {
-    let flight = nameInput.current.value;
-    const endpoint = 'https://data-live.flightradar24.com/clickhandler/'
+  function locate() { //Запрос данных с FlightRadar24
     const headers = {
       'User-Agent': 'https://github.com/derhuerst/fetch-flightradar24-flights'
     }
-    const url = endpoint + '?' + qs.stringify({ flight, version: '1.5' })
+    const url = 'https://data-live.flightradar24.com/zones/fcgi/feed.json'
     return fetch(url, {
       mode: 'cors',
       redirect: 'follow',
@@ -35,57 +25,64 @@ function App() {
       referrerPolicy: 'no-referrer'
     })
       .then(res => res.json())
-      .then(res => setData(res))
-      .then(res => setShow(true))
+      .then(res => {
+        const base = []
+        for (let key in res) {
+            if ((res[key][1] > 58.4 && res[key][1] < 65) && (res[key][2] > 57.2 && res[key][2] < 68.7)){
+              base.push(res[key])
+            }
+          }
+        return base
+      }
+      )
+      .then(res => setLocBase(res))
   }
-  console.log(az10[0].length)
-  const [viewport, setViewport] = React.useState({
+  
+  const [viewport, setViewport] = React.useState({  //Карта
     latitude: 0,
     longitude: 0,
     zoom: 5.8,
     mapboxApiAccessToken: 'pk.eyJ1Ijoib3ZlcmJyYXRzayIsImEiOiJja3dqODA3ZnMxZHA4Mm9udnlmOXdxMXQwIn0.s79NCW6WTu9I94Wv7MhNDA',
   });
   return (
-    <div>
+    <div className="window">
       <div className="radar">
         <div className="position">
-          <ReactMapGL {...viewport} width="100%" height="100%" mapStyle='mapbox://styles/mapbox/streets-v11' onViewportChange={(viewport) => setViewport(viewport)}>
-            {show && <Marker latitude={data.trail[0].lat} longitude={data.trail[0].lng}>
-            <Airplane/>
-            </Marker>}
+          <ReactMapGL id="666" {...viewport} width="100%" height="100%" mapStyle='mapbox://styles/mapbox/streets-v11' onViewportChange={(viewport) => setViewport(viewport)}>
+            {locBase.map(item => (<Marker key={item} latitude={item[1]} longitude={item[2]}>
+              <Airplane />
+            </Marker>))}
           </ReactMapGL>
-          {d10[0].map(item=>(<div key={item} className="range" style={{'width': `${d10[1][item]}px`, 'height': `${d10[1][item]}px`, borderWidth: '1px'}}></div>))}
-          {d50[0].map(item=>(<div key={item} className="range" style={{'width': `${d50[1][item]}px`, 'height': `${d50[1][item]}px`, borderWidth: '2px'}}></div>))}
-          {d100[0].map(item=>(<div key={item} className="range" style={{'width': `${d100[1][item]}px`, 'height': `${d100[1][item]}px`, borderWidth: '3px'}}></div>))}
-          {az10[0].map(item=>(<div key={item} className="azimut" style={{transform: `rotate(${az10[1][item]}deg)`, borderWidth: '1px', borderTop:'1px solid rgb(124, 124, 124, 0.568)'}}></div>))}
-          {az30[0].map(item=>(<div key={item} className="azimut" style={{transform: `rotate(${az30[1][item]}deg)`, borderWidth: '1px', borderTop:'1px solid rgb(0, 0, 0)'}}></div>))}
+          {d10[0].map(item => (<div key={item} className="range" style={{ 'width': `${d10[1][item]}px`, 'height': `${d10[1][item]}px`, borderWidth: '1px' }}></div>))}
+          {d50[0].map(item => (<div key={item} className="range" style={{ 'width': `${d50[1][item]}px`, 'height': `${d50[1][item]}px`, borderWidth: '2px' }}></div>))}
+          {d100[0].map(item => (<div key={item} className="range" style={{ 'width': `${d100[1][item]}px`, 'height': `${d100[1][item]}px`, borderWidth: '3px' }}></div>))}
+          {az10[0].map(item => (<div key={item} className="azimut" style={{ transform: `rotate(${az10[1][item]}deg)`, borderWidth: '1px', borderTop: '1px solid rgb(124, 124, 124, 0.568)' }}></div>))}
+          {az30[0].map(item => (<div key={item} className="azimut" style={{ transform: `rotate(${az30[1][item]}deg)`, borderWidth: '1px', borderTop: '1px solid rgb(0, 0, 0)' }}></div>))}
         </div>
         <div className="data">
-          {show &&
-            <table>
+          {locBase.map(item => (
+            <table key={item}>
               <thead>
                 <tr>
-                  <th colSpan="2">{data.identification.number.default}</th>
+                  <th colSpan="2">{item[16]}</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td>Высота</td>
-                  <td>{(Math.round((data.trail[0].alt)/0.3048))/10}</td>
+                  <td>{(Math.round((item[4]) / 0.33)) / 10}</td>
                 </tr>
                 <tr>
                   <td>Скорость</td>
-                  <td>{(data.trail[0].spd)*1,852}</td>
+                  <td>{Math.round((item[5]) * 1.87)}</td>
                 </tr>
                 <tr>
                   <td>Курс</td>
-                  <td>{data.trail[0].hd}</td>
+                  <td>{item[3]}</td>
                 </tr>
               </tbody>
             </table>
-          }
-          <input ref={nameInput}></input>
-          <button onClick={zapros}>Запрос</button>
+          ))}
         </div>
       </div>
     </div >
