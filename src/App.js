45936fd0a13b517fcdd15c99f1aa2f4f33381936
IlyaMinchenko <sir.over@yandex.ru>
2021-12-18 17:26:54 +0500

    v2.5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import React from "react";
 import './App.css'
 import ReactMapGL, { Marker} from 'react-map-gl';
@@ -8,43 +8,59 @@ import locate from "./locate";
 
 function App() {
   const [locBase, setLocBase] = useState([])
-  const [markerOn, setMarkerOn] = useState(false)
-  let markerStyle = {'fill' : 'red'}
-  let table = React.createRef()
-  let marker = React.createRef()
+  const [selectAirplane, setselectAirplane] = useState([])
+  const defaultColor ={'fill':'yellow'}
+  const selectColor = {'fill':'red'}
+  const click = React.createRef()
 
-  function clickHandler(event) {
-    
-    console.log(document.getElementsByClassName('mapboxgl-marker'))
-    console.log(event.target.innerHTML)
-  }
+  function clickHandler() {
+    async function select(){
+    let airplaneName = click.current.innerHTML;
+    let newlocBase = locBase.map(item=> {
+      if(item[17] === airplaneName){
+        item[19] = selectColor;
+        return item
+      }
+      else{
+        return item
+      }
+  }) 
+  await setselectAirplane(newlocBase)}
+console.log(selectAirplane)
+  select()
+}
 
   useEffect(() => {
+    let result;
     locate()
-    .then(res => setLocBase(res))
+    .then(res => result = res)
+    .then(res => result = result.map(item => item.concat(defaultColor)))
+    .then(res=> setLocBase(result))
   });
-  
+
   const [viewport, setViewport] = React.useState({  //Карта
-    latitude: 0,
-    longitude: 0,
+    latitude: 61.285304344909235,
+    longitude: 63.175818109252496,
     zoom: 5.8,
     mapboxApiAccessToken: 'pk.eyJ1Ijoib3ZlcmJyYXRzayIsImEiOiJja3dqODA3ZnMxZHA4Mm9udnlmOXdxMXQwIn0.s79NCW6WTu9I94Wv7MhNDA',
   });
+
 
   return (
       <div className="radar">
         <div className="position">
           <ReactMapGL id="666" {...viewport} width="100%" height="100%" mapStyle='mapbox://styles/mapbox/streets-v11' onViewportChange={(viewport) => setViewport(viewport)}>
-            {locBase.map(item => (<Marker key={item[0]} latitude={item[2]} longitude={item[3]} title={item[0]}>
-              <Airplane key={item[0]}/>
+            {locBase.map(item => (<Marker key={item[0]} latitude={item[2]} longitude={item[3]}>
+              <Airplane /><div>{item[17] || 'Без названия'}</div>
+              {/* <Airplane style={selectAirplane[item][19] || defaultColor}/><div>{item[17] || 'Без названия'}</div> */}
             </Marker>))}
           </ReactMapGL>
           <Radar/>
         </div>
         <div className="table">
           {locBase.map(item => (
-            <div key={item[0]} onClick={clickHandler} >
-                  <h1>{item[14] || 'Без названия'}</h1>
+            <div key={item[0]}>
+                  <h1  onClick={clickHandler} ref={click} >{item[17] || 'Без названия'}</h1>
                   <p>Высота: {(Math.round((item[5]) / 0.33)) / 10}</p>
                   <p>Скорость: {Math.round((item[6]) * 1.87)}</p>
                   <p>Курс: {item[4]}</p>
