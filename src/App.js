f54215f9fc14100f690394d77ed8e63c8f41b865
IlyaMinchenko <sir.over@yandex.ru>
2021-12-25 11:25:43 +0500

    v3.0
    Stable version
    Add input-area for inner latitude and longitude

--- src/App.js
+++ src/App.js
@@ -10,31 +10,39 @@ import Azimut from "./Azimut";
 import {defaultColorMarker, selectColorMarker} from "./MarkerColor";
 
 function App() {
-  const [showRadar, setShowRadar] = useState(false)
   const [locBase, setLocBase] = useState([])
   const [clickSelect, setClickSelect] = useState()
   const defaultColorTable = { 'background-color': 'rgba(211, 211, 211, 0.781)' }
   const selectColorTable = { 'background-color': 'rgb(214, 115, 69)' }
-  const lat1 = 0;
-  const lon1 = 0;
+  const latRef= React.createRef()
+  const lonRef= React.createRef()
+  const [lat, setLat] = useState(0)
+  const [lon, setLon] = useState(0)
 
   function clickHandler(event) {
     let airplaneName = event.currentTarget;
     airplaneName = airplaneName.outerText.slice(0, airplaneName.outerText.indexOf('\n'));
-    console.log(airplaneName);
     setClickSelect(airplaneName);
   }
 
-  function getRadar(event){
-    event.preventDefault();
-    if(event.target.elements.lat.value && event.target.elements.lon.value){
-        let lat = event.target.elements.lat.value;
-        let lon = event.target.elements.lon.value;
-        if((lat && lon) > 0){
-          setShowRadar(true)
+  function getRadar(){
+    if(latRef.current.value && lonRef.current.value){
+        let lat1 = +latRef.current.value;
+        let lon1 = +lonRef.current.value;
+        console.log(lat,lon)
+        if(lat1 >= -84 & lon1 >= -178){
+          setLat(lat1)
+          setLon(lon1)
+          console.log(`http://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=${lat1+2.6},${lon1-4.7},${lat1-3.8},${lon1+5.5}&adsb=1&air=1&array=1`)
+          setViewport({
+            latitude:lat1,
+            longitude:lon1,
+            zoom: 5.8,
+            mapboxApiAccessToken: 'pk.eyJ1Ijoib3ZlcmJyYXRzayIsImEiOiJja3dqODA3ZnMxZHA4Mm9udnlmOXdxMXQwIn0.s79NCW6WTu9I94Wv7MhNDA',
+          })
         }
         else {
-          alert('Введите положительные значения координат!')
+          alert('Введите верные координаты!')
         }
     } else {
       alert('Введите обе координаты!')
@@ -43,31 +51,30 @@ function App() {
 
   useEffect(() => {
     let result;
-    locate()
+    locate(lat, lon)
       .then(res => result = res)
-      .then(res => result.map(item => item.push(Dalnost(item, lat1, lon1))))
-      .then(res => result.map(item => item.push(Azimut(item, lat1, lon1))))
+      .then(res => result.map(item => item.push(Dalnost(item, lat, lon))))
+      .then(res => result.map(item => item.push(Azimut(item, lat, lon))))
       .then(res => setLocBase(result))
   });
 
   const [viewport, setViewport] = React.useState({  //Карта
-    latitude: lat1,
-    longitude: lon1,
+    latitude: lat,
+    longitude: lon,
     zoom: 5.8,
     mapboxApiAccessToken: 'pk.eyJ1Ijoib3ZlcmJyYXRzayIsImEiOiJja3dqODA3ZnMxZHA4Mm9udnlmOXdxMXQwIn0.s79NCW6WTu9I94Wv7MhNDA',
   });
 
   return (
     <>
-        <form action="" onSubmit={getRadar}>
-                    <input placeholder="Введите latitude" type="text" name="lat" id="lat" className="form" />
-                    <input placeholder="Введите longitude" type="text" name="lon" id="lon" className="form" />
-                    <button type="submit" className="btn">Ввод</button>
-                </form>
-    {showRadar ?  
+    <div className="input">
+    <input ref={latRef} defaultValue={0} placeholder="Введите latitude" type="text" name="lat" id="lat" className="form" />
+    <input ref={lonRef} defaultValue={0} placeholder="Введите longitude" type="text" name="lon" id="lon" className="form" />
+    <button onClick={getRadar} type="submit" className="btn">Ввод</button>
+    </div>
     <div className="radar">
       <div className="position">
-        <ReactMapGL id="666" {...viewport} width="100%" height="100%" mapStyle='mapbox://styles/mapbox/streets-v11' onViewportChange={(viewport) => setViewport(viewport)}>
+        <ReactMapGL id="666" {...viewport} width="100%" height="100%" mapStyle='mapbox://styles/mapbox/streets-v11' onViewportChange={setViewport}>
           {locBase.map(item => (<Marker key={item[0]} latitude={item[2]} longitude={item[3]}>
             <Airplane style={item[17] === clickSelect ? selectColorMarker(item[4]) : defaultColorMarker(item[4])} />
             <div>{item[17] || 'Без названия'}</div>
@@ -88,7 +95,6 @@ function App() {
         ))}
       </div>
     </div>
-    : null}
     </>
   );
 }
--- src/locate.js
+++ src/locate.js
@@ -1,7 +1,7 @@
 
-  export default function locate() { //Запрос данных с FlightRadar24
-    const url = 'http://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=63.9,58.4,57.5,68.7&adsb=1&air=1&array=1'
-    return fetch(url, {
+  export default function locate(lat1, lon1) { //Запрос данных с FlightRadar24
+    const url = `http://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=${lat1+2.6},${lon1-4.7},${lat1-3.8},${lon1+5.5}&adsb=1&air=1&array=1` //63.9,58.4,57.5,68.7
+        return fetch(url, {
       mode: 'cors',
       redirect: 'follow',
       referrer: 'no-referrer',
